b9f3071569307f4ba53561918eb21add8ae3d39c
Felipe Miller <fmvargas@protonmail.com>
2023-07-23 19:00:07 -0300

    exercicio V e VI

---
+++ exercicio_V.js
@@ -0,0 +1,9 @@
+const nomeEstudante = "Felipe Miller";
+let nota1 = 8;
+let nota2 = 9;
+let nota3 = 7;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+document.write(`O aluno ${nomeEstudante} teve uma média de ${media.toFixed(1)}`);
+
---
+++ exercicio_VI.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+
+let troca = a;
+a = b;
+b = troca;
+
+document.write("a: ", a);
+document.write(" <br>b: ", b);
